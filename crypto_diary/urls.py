21f482a96a60b8bc56fe80b03e26ba900ceffd50
asong nkemzi <nkemzia@gmail.com>
2022-05-05 07:01:38 -0400

    first commit

---
+++ crypto_diary/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('crypto.urls')),
+]
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
